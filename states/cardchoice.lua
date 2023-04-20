11b16dd2310dd550aeefe2ee479c6d580f0d3a16
Samy Panneton <samy.panneton.01@edu.cegeptr.qc.ca>
2023-04-20 17:20:34 -0400

    modif cardchoice

--- states/cardchoice.lua
+++ states/cardchoice.lua
@@ -17,125 +17,102 @@ local vec2 = require("vec2")
 local text = require("text")
 local sprite = require("sprite")
 
+local card = require("ui.card.card")
+
+-- card.children[1].on_click = function() end
+
+local c1 = card {
+    id = 1,
+    position = dim2(0.2, 0, 0.5, 0),
+    size = dim2(0.25, 0, 0.3, 0),
+    anchor = vec2.new(0.5, 0.5),
+}
+
+c1.children[1].on_click[1] = function() 
+    transition(root, require("states.progression"), 1)
+end
+
+local c2 = card {
+    id = 1,
+    position = dim2(0.5, 0, 0.5, 0),
+    size = dim2(0.25, 0, 0.3, 0),
+    anchor = vec2.new(0.5, 0.5),
+}
+
+c2.children[1].on_click[1] = function() 
+    transition(root, require("states.progression"), 1)
+end
+
+local c3 = card {
+    id = 1,
+    position = dim2(0.8, 0, 0.5, 0),
+    size = dim2(0.25, 0, 0.3, 0),
+    anchor = vec2.new(0.5, 0.5),
+}
+
+c3.children[1].on_click[1] = function() 
+    transition(root, require("states.progression"), 1)
+end
+
 root:add_children{
     sprite {
         image = assets.sprites.game,
         position = dim2(0, 0, 0, 0),
         size = dim2(1, 0, 1, 0),
-        scaling = SCALING.STRETCH,
+        scaling = SCALING.CENTER_OVERFLOW,
 
         children = {
-            button {
-                position = dim2(0.2, 0, 0.5, 0),
-                size = dim2(0.25, 0, 0.3, 0),
+            frame {
+                position = dim2(0.5, 0, 0.17, 0),
+                size = dim2(0.65, 0, 0.3, 0),
                 anchor = vec2.new(0.5, 0.5),
-
-                on_click = {
-                    -- left button
-                    [1] = function()
-                        -- transition(root, require("states.progression"), 1)
-                    end,
-                    -- right button
-                    [2] = function()
-                        -- do nothing
-                    end
-                },
-
-                children = {
-                    frame {
-                        color = color.new(1, 0, 0, 1),
-                    },
-                    text {
-                            position = dim2(0.5, 0, 0.5, 0),
-                            text = "Ta",
-                            font = assets.fonts.roboto[42],
-                            x_align = ALIGN.CENTER_X,
-                            y_align = ALIGN.CENTER_Y,
-                            color = color.new(0, 0, 0, 1),
-                        }  
-                }
+                color = color.new(1, 1, 1, 0.5),
+                scaling = SCALING.CENTER,
             },
-            button {
-                position = dim2(0.5, 0, 0.5, 0),
-                size = dim2(0.25, 0, 0.3, 0),
-                anchor = vec2.new(0.5, 0.5),
-
-                on_click = {
-                    -- left button
-                    [1] = function()
-                        -- transition(root, require("states.progression"), 1)
-                    end,
-                    -- right button
-                    [2] = function()
-                        -- do nothing
-                    end
-                },
-
-                children = {
-                    frame {
-                        color = color.new(1, 0, 0, 1),
-                    },
-                    text {
-                        position = dim2(0.5, 0, 0.5, 0),
-                        text = "mère",
-                        font = assets.fonts.roboto[42],
-                        x_align = ALIGN.CENTER_X,
-                        y_align = ALIGN.CENTER_Y,
-                       color = color.new(0, 0, 0, 1),
-                    } 
-                }
+            text {
+                position = dim2(0.5, 0, 0.1, 0),
+                text = "YOU LOST",
+                font = assets.fonts.roboto[100],
+                x_align = ALIGN.CENTER_X,
+                y_align = ALIGN.CENTER_Y,
+                color = color.new(0, 0, 0, 1),
             },
-            button {
-                position = dim2(0.8, 0, 0.5, 0),
-                size = dim2(0.25, 0, 0.3, 0),
-                anchor = vec2.new(0.5, 0.5),
-
-                on_click = {
-                    -- left button
-                    [1] = function()
-                        -- transition(root, require("states.progression"), 1)
-                    end,
-                    -- right button
-                    [2] = function()
-                        -- do nothing
-                    end
-                },
-
-                children = {
-                    frame {
-                        color = color.new(1, 0, 0, 1),
-                    },
-                    text {
-                        position = dim2(0.5, 0, 0.5, 0),
-                        text = "la ...",
-                        font = assets.fonts.roboto[42],
-                        x_align = ALIGN.CENTER_X,
-                        y_align = ALIGN.CENTER_Y,
-                        color = color.new(0, 0, 0, 1),
-                    }  
-                }
+            text {
+                position = dim2(0.5, 0, 0.25, 0),
+                text = "choose a card",
+                font = assets.fonts.roboto[100],
+                x_align = ALIGN.CENTER_X,
+                y_align = ALIGN.CENTER_Y,
+                color = color.new(0, 0, 0, 1),
             },
-            button {
-                position = dim2(0.9, 0, 0.9, 0),
-                size = dim2(0.1, 0, 0.1, 0),
+            c1,
+            c2,
+            c3,
+            sprite {
+                image = assets.sprites.sword_button,
+                position = dim2(0.8, 0, 0.9, 0),
+                size = dim2(0.4, 0, 0.1, 0),
                 anchor = vec2.new(0.5, 0.5),
+                scaling = SCALING.CENTER,
 
-                on_click = {
-                    -- left button
-                    [1] = function()
-                       -- transition(root, require("states.progression"), 1)
-                    end,
-                    -- right button
-                    [2] = function()
-                        -- do nothing
-                    end
-                },
                 children = {
-                    frame {
-                        color = color.new(1, 0, 0, 1),
+                    button {
+                        position = dim2(0.5, 0, 0.5, 0),
+                        size = dim2(0.7, 0, 1, 0),
+                        anchor = vec2.new(0.5, 0.5),
+                        
+                        on_click = {
+                            -- left button
+                            [1] = function()
+                                
+                                transition(root, require("states.progression"), 1)
+                            end,
+                            
+                            
+                        },
                     },
                     text {
-                        position = dim2(0.5, 0, 0.5, 0),
+                        position = dim2(0.5, 0, 0.5, -8),
                         text = "Skip",
                         font = assets.fonts.roboto[42],
                         x_align = ALIGN.CENTER_X,
