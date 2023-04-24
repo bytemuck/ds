14392a3b3346fa8796e5dc154db60c1efb3e311d
Nathan Pinard <nathan.pinard.01@edu.cegeptr.qc.ca>
2023-04-24 10:42:19 -0400

    deck helper functions

--- persistent/save.lua
+++ persistent/save.lua
@@ -19,5 +19,23 @@ return {
         if type(encoded) == "string" then
             love.filesystem.write(savepath, encoded)
         end
+    end,
+
+    shuffle_deck = function ()
+        for i = #persistent.deck, 2, -1 do
+            local j = math.random(i)
+            persistent.deck[i], persistent.deck[j] = persistent.deck[j], persistent.deck[i]
+        end
+    end,
+
+    draw_deck = function ()
+        local pop = persistent.deck[#persistent.deck] -- Take last item.
+        persistent.deck[#persistent.deck] = nil -- Set it to null
+        persistent.deck = { pop, table.unpack(persistent.deck) } -- drawed card is now on the bottom
+        return pop -- return drawed card
+    end,
+
+    push = function (id)
+        persistent.deck[#persistent.deck] = id
     end
 }
