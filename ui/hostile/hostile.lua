541a5c86cc9033fc9d2239e183d9d5c5504f2fbf
Nathan Pinard <nathan.pinard.01@edu.cegeptr.qc.ca>
2023-04-27 16:28:48 -0400

    Remove feature where hostile kill themselves

--- ui/hostile/hostile.lua
+++ ui/hostile/hostile.lua
@@ -25,7 +25,7 @@ return element.make_new {
     cctr = function(self)
         self.health = self.health or 3
         self.intention = self.intention or INTENTION.ATTACK -- do damage to player
-        self.intention_value = self.intention_value or 3 -- 3 damage
+        self.intention_value = self.intention_value or 3    -- 3 damage
     end,
 
     postcctr = function(self)
@@ -55,8 +55,8 @@ return element.make_new {
                         sprite {
                             image = assets.sprites.random,
                             position = dim2(0, 0, 1, 0), -- bottom left
-                            size = dim2(0, 32, 0, 32), -- 32px 32px
-                            anchor = vec2.new(0, 1), -- bottom left
+                            size = dim2(0, 32, 0, 32),   -- 32px 32px
+                            anchor = vec2.new(0, 1),     -- bottom left
                             children = {
                                 self.health_text
                             },
@@ -64,16 +64,9 @@ return element.make_new {
                         sprite {
                             image = assets.sprites.x_button,
                             position = dim2(0.5, 0, 0, 0), -- top center
-                            size = dim2(0, 32, 0, 32), -- 32px 32px
-                            anchor = vec2.new(0.5, 0), -- bottom left
+                            size = dim2(0, 32, 0, 32),     -- 32px 32px
+                            anchor = vec2.new(0.5, 0),     -- bottom left
                         },
-                        button {
-                            on_click = {
-                                [1] = function()
-                                    self:take_damage(1)
-                                end,
-                            },
-                        }
                     } }
                 }
             }
@@ -90,14 +83,14 @@ return element.make_new {
             self.health_text.text_objs = self.health_text.update_text(tostring(self.health))
         end,
         die = function(self)
-            flux.to(self.children[1].children[1].size, 0.5, dim2(0,0,0,0).vals):ease("circout"):oncomplete(function()
+            flux.to(self.children[1].children[1].size, 0.5, dim2(0, 0, 0, 0).vals):ease("circout"):oncomplete(function()
                 self.children = {}
                 self.parent:recalc()
             end)
         end,
         generate_turn = function(self)
             self.intention = INTENTION.ATTACK -- we should generate and intention
-            self.intention_value = 3 -- should be a generated object used by intention
+            self.intention_value = 3          -- should be a generated object used by intention
         end,
         play_turn = function(self, player)
             if self.intention == INTENTION.ATTACK then
